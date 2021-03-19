13915b08c52cde907430ff837fd501a7d0b29465
Cristian Furtado <cristiansafurtado@gmail.com>
2021-03-19 19:07:16 -0300

    Co-authored-by: Cristian Furtado <cristiansafurtado@gmail.com> Co-authored-by: jplyra <jplyra97@gmail.com>
    Signed-off-by: jplyra <jplyra97@gmail.com>
    Signed-off-by: Cristian Furtado <cristiansafurtado@gmail.com>

--- src/api/api.py
+++ src/api/api.py
@@ -24,44 +24,33 @@ def index():
 def resultado():
     #recebemos um json do request com {nome, uf e partido}
     requested_json = request.get_json()
-    name_filter = requested_json["nome"]
-    uf_filter = requested_json["uf"]
-    party_filter = requested_json["partido"]
+    name_filter = str.lower(requested_json["nome"])
+    uf_filter = str.lower(requested_json["uf"])
+    party_filter = str.lower(requested_json["partido"])
 
-    #criar uam lista vazia com todos os candidatos 
-    #adicionar na lista de acordo com os filtros e verificações
+    # Cria uma lista vazia e preenche com os objetos salvos de Deputy
     all_deputies = []
+    for item in Deputy.objects:
+        all_deputies.append(item)
 
-    #verificar se o cada filtro não é nulo nem vazio
-    if name_filter:
-        #colocar em lower case pra não fazer diferença na comparação
-        name_string = str.lower(name_filter)
-        #para cada item dentro da lista dos deputados do banco de dados (foreach)
-        for item in Deputy.objects:
-            #deixar as strings em lower case pra poder comparar com o filtro
-            item_name = str.lower(item.name)
-            if(name_string in item_name) and (item not in all_deputies):
-                #caso o filtro bata e já não tenha adicionado esse item na lista, adicionar
-                all_deputies.append(item)
 
-    if uf_filter:
-        uf_string = str.lower(uf_filter)
-        for item in Deputy.objects:
-            item_uf = str.lower(item.federative_unity)
-            if (uf_string in item_uf) and (item not in all_deputies):
-                all_deputies.append(item)
+    # Filtra os resultados da pesquisa
+    for deputy in Deputy.objects:
+        if str.lower(deputy.name) == name_filter or name_filter == "":
+            if str.lower(deputy.federative_unity) ==  uf_filter or uf_filter == "":
+                if str.lower(deputy.party) == party_filter or party_filter == "":
+                    continue
+                else:
+                    all_deputies.remove(deputy)
+            else:
+                all_deputies.remove(deputy)
+        else:
+            all_deputies.remove(deputy)
 
-    if party_filter:
-        party_string = str.lower(party_filter)
-        for item in Deputy.objects:
-            item_party = str.lower(item.party)
-            if(party_string in item_party) and (item not in all_deputies):
-                all_deputies.append(item)
-
-    #ordenar os deputados por nome
+    # Ordena os deputados por nome
     sorted_list = sorted(all_deputies, key=attrgetter('name'))
 
-    #criar um json com todos os deputados encontrados e ordenados
+    # Cria um json com todos os deputados encontrados e já ordenados
     full_json = {}
     cont = 0
     for deputy in sorted_list:
