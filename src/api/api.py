79fc8f1615832df4fa79622960568ece7cddbfb5
Gabriel Albino <enggabrielalbino@gmail.com>
2021-03-24 19:54:21 -0300

    7 Corrigindo json do retorno de deputados

--- src/api/api.py
+++ src/api/api.py
@@ -12,7 +12,7 @@ def index():
     sorted_list = sorted(Deputy.objects, key=attrgetter('name'))
 
     for deputy in sorted_list:
-        temp_json = deputy.to_json(deputy)
+        temp_json = deputy.to_json()
         full_json.append(temp_json)
 
     return jsonify(full_json)
@@ -52,55 +52,18 @@ def resultado():
     full_json = []
 
     for deputy in sorted_list:
-        temp_json = deputy.to_json(deputy)
+        temp_json = deputy.to_json()
         full_json.append(temp_json)
 
     # Retorna no formato JSON a lista de objetos full_json
     return jsonify(full_json)
 
-#Pegar as duas noticias mais recentes do nosso banco de dados
-@api.route('/news')
-def news():
-    all_news = News.objects
-    
-    #Ordenar a lista de acordo com a data.
-    sorted_list = sorted(all_news, key=attrgetter('update_date'))
-    news_1 = sorted_list[0].to_json(sorted_list[0])
-    news_2 = sorted_list[1].to_json(sorted_list[1])
-    json_full = {
-        'news_01':news_1,
-        'news_02':news_2
-    }
-
-    return json_full
-
-@api.route('/<id>')
+@api.route('/deputies/<id>')
 def profile(id):
-    profile_json = {}
-    
     for profile in Deputy.objects:
       #Adicionar informacoes que estao comentadas
         if int(id) == int(profile.id):
-            
-            profile_json["photo_url"] = profile.name
-            #Informacoes Pessoais
-            profile_json["full_name"] = profile.full_name
-            profile_json["party"] = profile.party
-            profile_json["federative_unity"] = profile.federative_unity
-            # profile_json["age"] = profile.age
-            profile_json["birth_date"] = profile.birth_date
-            #Informacoes do Gabinete
-            # profile_json["room_number"] = profile.room_number
-            # profile_json["floor"] = profile.floor
-            # profile_json["building"] = profile.building
-            # profile_json["telephone"] = profile.telephone
-            # profile_json["email"] = profile.email
-            #Redes Sociais
-            profile_json["facebook_username"] = profile.facebook_username
-            profile_json["twitter_username"] = profile.twitter_username
-            profile_json["instagram_username"] = profile.instagram_username
-            
-            return profile_json
+            return profile.to_json()
         
     return {} 
         
