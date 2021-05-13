6cdfe53993fe41049739c7b266df3f7de917105b
lmmLucasMachado <lmlucasmachadom@gmail.com>
2021-05-13 19:21:13 -0300

    Adicionando testes

--- src/test.py
+++ src/test.py
@@ -64,59 +64,5 @@ class EtlTests(unittest.TestCase):
     self.assertEqual(200 , request.status_code)
     self.assertEqual(response , request.data.decode())
 
-  def test_parties_status(self):
-    request = self.client.get(url_for('api.parties'))
-    self.assertEqual(200 , request.status_code)
-
-  def test_get_votes_status(self):
-    request = self.client.get(url_for('api.get_votes'))
-    self.assertEqual(200 , request.status_code)
-
-  def test_expenses_status(self):
-    request = self.client.get(url_for('api.expenses'))
-    self.assertEqual(200 , request.status_code)
-
-  def test_get_all_propositions_status(self):
-    request = self.client.get(url_for('api.get_all_propositions'))
-    self.assertEqual(200 , request.status_code)
-
-  def test_delet_deputados_status(self):
-    data_expected = 'Deputados apagados com sucesso'
-
-    request = self.client.get(url_for('api.remover_deputados'))
-    self.assertNotEqual(data_expected , request.data.decode())
-
-  def tearDown(self):
-      self.context.pop()
-
-  @requests_mock.Mocker()
-  def test_atualizar_deputados(self, request_mock):
-    url = 'https://dadosabertos.camara.leg.br/arquivos/deputados/json/deputados.json'
-    data = {
-      "dados": [
-        {
-          "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/2",
-          "nome": "LOPES GAMA",
-          "idLegislaturaInicial": 1,
-          "idLegislaturaFinal": 2,
-          "nomeCivil": "Caetano Maria Lopes Gama",
-          "siglaSexo": "M",
-          "urlRedeSocial": [],
-          "urlWebsite": [],
-          "dataNascimento": "1795-08-05",
-          "dataFalecimento": "1864-06-21",
-          "ufNascimento": "PE",
-          "municipioNascimento": "Recife"
-        }
-      ]
-    }
-
-    data_expected = 'Done. Use /deputies to get all deputies in data base.'
-
-    request_mock.get(url, json=data)
-    request = self.client.get(url_for('api.atualizar_deputados'))
-    self.assertEqual(200 , request.status_code)
-    self.assertEqual(data_expected, request.data.decode())
-
 if __name__=='__main__':
     unittest.main()
