c4c93b530362496c6f91281e03146afa76fb4771
lmmLucasMachado <lmlucasmachadom@gmail.com>
2021-05-13 19:25:42 -0300

    Corrigindo testes por causa da refatoraçao

--- src/test.py
+++ src/test.py
@@ -22,60 +22,6 @@ class EtlTests(unittest.TestCase):
     self.assertEqual('ETL Camara' , request.data.decode())
       
   @requests_mock.Mocker()
-  def test_deputies_only(self, request_mock):
-    url = 'https://dadosabertos.camara.leg.br/api/v2/deputados/204554'
-    data = {
-      "cpf": "36607606504",
-      "dataFalecimento": null,
-      "dataNascimento": "1965-02-13",
-      "escolaridade": "Superior Incompleto",
-      "id": 204554,
-      "municipioNascimento": "Salvador",
-      "nomeCivil": "JOSE ABILIO SILVA DE SANTANA",
-      "redeSocial": [],
-      "sexo": "M",
-      "ufNascimento": "BA",
-      "ultimoStatus": {
-          "condicaoEleitoral": "Titular",
-          "data": "2019-02-01T11:45",
-          "descricaoStatus": null,
-          "email": "dep.abiliosantana@camara.leg.br",
-          "gabinete": {
-              "andar": "5",
-              "email": "dep.abiliosantana@camara.leg.br",
-              "nome": "531",
-              "predio": "4",
-              "sala": "531",
-              "telefone": "3215-5531"
-          },
-          "id": 204554,
-          "idLegislatura": 56,
-          "nome": "Abilio Santana",
-          "nomeEleitoral": "Abilio Santana",
-          "siglaPartido": "PL",
-          "siglaUf": "BA",
-          "situacao": "Exercicio",
-          "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/204554",
-          "uriPartido": "https://dadosabertos.camara.leg.br/api/v2/partidos/37906",
-          "urlFoto": "https://www.camara.leg.br/internet/deputado/bandep/204554.jpg"
-      },
-      "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/204554",
-      "urlWebsite": null
-    }
-    
-    self.assertEqual(self.app.ver_deputado('204554'), return_json)
-    self.assertEqual('JOSE ABILIO SILVA DE SANTANA', return_json['nomeCivil'])
-    self.assertNotEqual('Joao da Silva', return_json['nomeCivil'])
-
-    request_mock.get(url, json=data)
-    request_mock.status_code(200)
-
-    request = self.client.get(url_for('api.federative_unities'))
-
-    self.assertEqual(200 , request.status_code)
-    self.assertEqual(response , request.data.decode())
-
-  @requests_mock.Mocker()
   def test_federative_unities(self, request_mock):
     url =  'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
     data = [
