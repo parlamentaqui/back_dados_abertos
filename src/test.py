c0d671123316d8920e934766195ccac63eda4321
lmmLucasMachado <lmlucasmachadom@gmail.com>
2021-05-15 12:34:54 -0300

    Refaorando e finalizando testes

--- src/test.py
+++ src/test.py
@@ -1,10 +1,19 @@
+
 import unittest
-from flask import url_for
-import requests_mock
 from app import app
+from mongoengine import connect
+from flask import Flask
+import requests_mock
+import os
 
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+PORT = os.getenv('PORT')
 
-class EtlTests(unittest.TestCase):
+class AppTests(unittest.TestCase):
 
   def setUp(self):
     self.app = app
@@ -14,12 +23,32 @@ class EtlTests(unittest.TestCase):
     self.client = self.app.test_client()
 
   def test_index_status(self):
-    request = self.client.get(url_for('/'))
+    request = self.client.get('/')
     self.assertEqual(200 , request.status_code)
-      
+
   def test_index(self):
-    request = self.client.get(url_for('/'))
+    request = self.client.get('/')
     self.assertEqual('ETL Camara' , request.data.decode())
+    self.assertGreaterEqual(len(request.data.decode()),2)
+
+  def test_fake_status(self):
+    request = self.client.get('/not_exist')
+    self.assertEqual(404 , request.status_code)
+
+  def tearDown(self):
+    self.context.pop()
+
+
+class EtlTests(unittest.TestCase):
+  
+  connect(DB_NAME, host=f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?authSource=admin')
+
+  def setUp(self):
+    self.app = app
+    self.app.testing = True
+    self.context = self.app.test_request_context()
+    self.context.push()
+    self.client = self.app.test_client()
       
   @requests_mock.Mocker()
   def test_federative_unities(self, request_mock):
@@ -60,38 +89,39 @@ class EtlTests(unittest.TestCase):
     request_mock.get(url, json=data)
     request_mock.status_code(200)
 
-    request = self.client.get(url_for('api.federative_unities'))
+    request = self.client.get('/api/federative_unities')
 
     self.assertEqual(200 , request.status_code)
     self.assertEqual(response , request.data.decode())
 
   def test_parties_status(self):
-    request = self.client.get(url_for('api.parties'))
+    request = self.client.get('/api/parties')
     self.assertEqual(200 , request.status_code)
 
   def test_get_votes_status(self):
-    request = self.client.get(url_for('api.get_votes'))
+    request = self.client.get('/api/get_votes')
     self.assertEqual(200 , request.status_code)
 
   def test_expenses_status(self):
-    request = self.client.get(url_for('api.expenses'))
+    request = self.client.get('/api/expenses')
     self.assertEqual(200 , request.status_code)
 
   def test_get_all_propositions_status(self):
-    request = self.client.get(url_for('api.get_all_propositions'))
+    request = self.client.get('/api/get_all_propositions')
     self.assertEqual(200 , request.status_code)
 
   def test_delet_deputados_status(self):
     data_expected = 'Deputados apagados com sucesso'
 
-    request = self.client.get(url_for('api.remover_deputados'))
+    request = self.client.get('/api/remover_deputados')
+    self.assertEqual(200 , request.status_code)
     self.assertNotEqual(data_expected , request.data.decode())
 
   def tearDown(self):
       self.context.pop()
 
   @requests_mock.Mocker()
-  def test_atualizar_deputados(self, request_mock):
+  def test_update_deputies(self, request_mock):
     url = 'https://dadosabertos.camara.leg.br/arquivos/deputados/json/deputados.json'
     data = {
       "dados": [
@@ -115,9 +145,54 @@ class EtlTests(unittest.TestCase):
     data_expected = 'Done. Use /deputies to get all deputies in data base.'
 
     request_mock.get(url, json=data)
-    request = self.client.get(url_for('api.atualizar_deputados'))
+    request = self.client.get('/api/atualizar_deputados')
     self.assertEqual(200 , request.status_code)
     self.assertEqual(data_expected, request.data.decode())
+
+  @requests_mock.Mocker()
+  def test_update_votes(self, request_mock):
+    url = 'https://dadosabertos.camara.leg.br/api/v2/votacoes?ordem=DESC&ordenarPor=dataHoraRegistro'
+    data = {
+    "dados": [
+        {
+            "id": "2250723-31",
+            "uri": "https://dadosabertos.camara.leg.br/api/v2/votacoes/2250723-31",
+            "data": "2021-05-13",
+            "dataHoraRegistro": "2021-05-13T16:10:19",
+            "siglaOrgao": "SECAP(SGM)",
+            "uriOrgao": "https://dadosabertos.camara.leg.br/api/v2/orgaos/100001",
+            "uriEvento": null,
+            "proposicaoObjeto": null,
+            "uriProposicaoObjeto": null,
+            "descricao": "Realizar o encaminhamento do PL-2228/2020 à CSSF (tramitação simultânea), em razão da aprovação de requerimento de urgência.",
+            "aprovacao": 1
+        },
+        {
+            "id": "2250723-30",
+            "uri": "https://dadosabertos.camara.leg.br/api/v2/votacoes/2250723-30",
+            "data": "2021-05-13",
+            "dataHoraRegistro": "2021-05-13T16:10:14",
+            "siglaOrgao": "SECAP(SGM)",
+            "uriOrgao": "https://dadosabertos.camara.leg.br/api/v2/orgaos/100001",
+            "uriEvento": null,
+            "proposicaoObjeto": null,
+            "uriProposicaoObjeto": null,
+            "descricao": "Realizar o encaminhamento do PL-2228/2020 à CFT (tramitação simultânea), em razão da aprovação de requerimento de urgência.",
+            "aprovacao": 1
+        }
+      ]
+    }
+
+    data_expected = 'Done! Use api/get_votes url to get all votes in database'
+
+    request_mock.get(url, json=data)
+    request = self.client.get('/api/update_votes')
+    self.assertEqual(200 , request.status_code)
+    self.assertEqual(data_expected, request.data.decode())
+
+  def tearDown(self):
+    self.context.pop()
+
 
 if __name__=='__main__':
     unittest.main()
